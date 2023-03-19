55e1d4a8203fcd459b0e2828c29b880c7f42a60c
Espresso521 <kotaku20220424@gmail.com>
2023-03-19 13:05:50 +0900

    1. Use github.com/gorilla/mux for router

--- src/main.go
+++ src/main.go
@@ -7,6 +7,8 @@ package main
 import (
 	"log"
 	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
 func serveHome(w http.ResponseWriter, r *http.Request) {
@@ -22,17 +24,35 @@ func serveHome(w http.ResponseWriter, r *http.Request) {
 	http.ServeFile(w, r, "home.html")
 }
 
+func serveTest(w http.ResponseWriter, r *http.Request) {
+	log.Println(r.URL)
+	if r.URL.Path != "/test" {
+		http.Error(w, "Not found", http.StatusNotFound)
+		return
+	}
+	if r.Method != http.MethodGet {
+		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+		return
+	}
+	http.ServeFile(w, r, "test.html")
+}
+
 func main() {
 	hub := newHub()
 	go hub.run()
-	http.HandleFunc("/", serveHome)
-	http.HandleFunc("/ws", func(w http.ResponseWriter, r *http.Request) {
+
+	router := mux.NewRouter()
+
+	
+	router.HandleFunc("/", serveHome)
+	router.HandleFunc("/test", serveTest).Methods("GET")
+	router.HandleFunc("/ws", func(w http.ResponseWriter, r *http.Request) {
 		serveWs(hub, w, r)
 	})
 
 	log.Println("Server start")
 	//err := http.ListenAndServeTLS("0.0.0.0:5211", "cert.cer", "private.key", nil)
-	err := http.ListenAndServe(":5211", nil)
+	err := http.ListenAndServe(":5211", router)
 	if err != nil {
 		log.Fatal("ListenAndServeTLS: ", err)
 	}
