5c852c95391bffe9e6ca71137226dc5118091b18
Espresso521 <kotaku20220424@gmail.com>
2023-03-20 10:12:08 +0900

    1. Add Middleware to print log 2. Modify .gitignore

--- src/main.go
+++ src/main.go
@@ -11,6 +11,52 @@ import (
 	"github.com/gorilla/mux"
 )
 
+type Middleware func(http.HandlerFunc) http.HandlerFunc
+
+// 记录每个URL请求的执行时长
+func Logging() Middleware {
+
+    // 创建中间件
+    return func(f http.HandlerFunc) http.HandlerFunc {
+
+        // 创建一个新的handler包装http.HandlerFunc
+        return func(w http.ResponseWriter, r *http.Request) {
+
+            // 中间件的处理逻辑
+						log.Println("Logging: " + r.URL.Path) 
+ 
+            // 调用下一个中间件或者最终的handler处理程序
+            f(w, r)
+        }
+    }
+}
+
+// 验证请求用的是否是指定的HTTP Method，不是则返回 400 Bad Request
+func Method(m string) Middleware {
+
+	return func(f http.HandlerFunc) http.HandlerFunc {
+
+			return func(w http.ResponseWriter, r *http.Request) {
+
+					if r.Method != m {
+							http.Error(w, http.StatusText(http.StatusBadRequest), http.StatusBadRequest)
+							return
+					}
+
+					f(w, r)
+			}
+	}
+}
+
+// 把应用到http.HandlerFunc处理器的中间件
+// 按照先后顺序和处理器本身链起来供http.HandleFunc调用
+func Chain(f http.HandlerFunc, middlewares ...Middleware) http.HandlerFunc {
+	for _, m := range middlewares {
+			f = m(f)
+	}
+	return f
+}
+
 func serveHome(w http.ResponseWriter, r *http.Request) {
 	log.Println(r.URL)
 	if r.URL.Path != "/" {
@@ -35,6 +81,21 @@ func serveTest(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 	http.ServeFile(w, r, "test.html")
+	log.Println(r.URL.Path + " END") 
+}
+
+func appStartTime(w http.ResponseWriter, r *http.Request) {
+	log.Println(r.URL)
+	if r.URL.Path != "/api/v/1.0/app/start" {
+		http.Error(w, "Not found", http.StatusNotFound)
+		return
+	}
+	if r.Method != http.MethodPost {
+		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+		return
+	}
+	http.ServeFile(w, r, "test.html")
+	log.Println(r.URL.Path + " END") 
 }
 
 func main() {
@@ -43,15 +104,16 @@ func main() {
 
 	router := mux.NewRouter()
 
-	
+	// regist router
 	router.HandleFunc("/", serveHome)
-	router.HandleFunc("/test", serveTest).Methods("GET")
+	router.HandleFunc("/test", Chain(serveTest, Method("GET"), Logging())).Methods("GET")
+	// /api/v/1.0/app/start
+	router.HandleFunc("/api/v/1.0/app/start", Chain(appStartTime, Method("POST"), Logging())).Methods("POST")
 	router.HandleFunc("/ws", func(w http.ResponseWriter, r *http.Request) {
 		serveWs(hub, w, r)
 	})
 
 	log.Println("Server start")
-	//err := http.ListenAndServeTLS("0.0.0.0:5211", "cert.cer", "private.key", nil)
 	err := http.ListenAndServe(":5211", router)
 	if err != nil {
 		log.Fatal("ListenAndServeTLS: ", err)
