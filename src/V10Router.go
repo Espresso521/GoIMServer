faa832e2402ffba20290f85b5f8749eb9b6c923b
Espresso521 <kotaku20220424@gmail.com>
2023-03-28 22:02:47 +0900

    1. Use Gin to Refactor, Good!!

---
+++ src/V10Router.go
@@ -0,0 +1,87 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"os"
+	"time"
+
+	"github.com/gin-gonic/gin"
+)
+
+type V10Router struct {
+	router *gin.RouterGroup
+}
+
+func (r *V10Router) addV10Router() {
+  r.router.GET("/route", func(c *gin.Context) {
+    c.JSON(http.StatusOK, gin.H{"message": "Custom route"})
+  })
+
+	// 设置一个get请求的路由，url为/ping, 处理函数（或者叫控制器函数）是一个闭包函数。
+	r.router.GET("/ping", func(c *gin.Context) {
+			// 通过请求上下文对象Context, 直接往客户端返回一个json
+		c.JSON(http.StatusOK, gin.H{
+			"message": "pong",
+		})
+	})
+
+	r.router.POST("/app/start/", func(c *gin.Context) {
+		c.Request.ParseMultipartForm(1024*1024)
+		for key, values := range c.Request.PostForm {
+			log.Printf("Form field %q, Values %q\n", key, values)
+	  }
+	  c.JSON(http.StatusOK, getAnswer())
+  })
+
+	r.router.POST("/run/start/", func(c *gin.Context) {
+		c.Request.ParseMultipartForm(1024*1024)
+	  c.JSON(http.StatusOK, ReceiveFile(c.Writer, c.Request))
+  })
+
+	r.router.POST("/run/finish/", func(c *gin.Context) {
+		c.Request.ParseMultipartForm(1024*1024)
+	  c.JSON(http.StatusOK, ReceiveFile(c.Writer, c.Request))
+  })
+
+	r.router.POST("/run/status/", func(c *gin.Context) {
+		c.Request.ParseMultipartForm(1024*1024)
+		for key, values := range c.Request.PostForm {
+			log.Printf("Form field %q, Values %q\n", key, values)
+	  }
+	  c.JSON(http.StatusOK, getAnswer())
+  })
+}
+
+func ReceiveFile(w http.ResponseWriter, r *http.Request) string {
+	file, header, err := r.FormFile("image")
+	if err != nil {
+			panic(err)
+	}
+	defer file.Close()
+	fmt.Printf("File name %s\n", header.Filename)
+
+	for key, values := range r.Form {
+		log.Printf("Form field %q, Values %q\n", key, values)
+ }
+
+	// create a new file in the local file system
+	f, err := os.OpenFile("./uploads/"+header.Filename, os.O_WRONLY|os.O_CREATE, 0666)
+	if err != nil {
+		return fmt.Sprintf("{'auth_token':'a48396e4f5bec65ddd415cb802cd37be7a5784cae', 'error':'%s'}", err.Error())
+	}
+	defer f.Close()
+
+	// copy the uploaded file data to the new file
+	_, err = io.Copy(f, file)
+	if err != nil {
+			return fmt.Sprintf("{'auth_token':'a48396e4f5bec65ddd415cb802cd37be7a5784cae', 'error':'%s'}", err.Error())
+	}
+
+	fStat, _ := f.Stat()
+
+	// return a success message to the client
+	return fmt.Sprintf("{'auth_token':'a48396e4f5bec65ddd415cb802cd37be7a5784cae', 'time':'%s', 'extra':'File uploaded successfully: file name is %v, file size is %v bytes'}", time.Now(), f.Name(), fStat.Size())
+}
--- src/main.go
+++ src/main.go
@@ -7,136 +7,45 @@ package main
 import (
 	"encoding/json"
 	"fmt"
-	"io"
 	"log"
 	"net/http"
-	"os"
 	"time"
 
-	"github.com/gorilla/mux"
+	"github.com/gin-contrib/static"
+	"github.com/gin-gonic/gin"
 )
 
-func ReceiveFile(w http.ResponseWriter, r *http.Request) {
-	r.ParseMultipartForm(32 << 20) 
+func main() {
+	hub := newHub()
+	go hub.run()
 
-	file, header, err := r.FormFile("image")
-	if err != nil {
-			panic(err)
-	}
-	defer file.Close()
-	fmt.Printf("File name %s\n", header.Filename)
+	defaultRouter := gin.Default()
+	addDefaultRouter(defaultRouter, hub)
+  // Optionally, set up additional routes using a custom router group
+  v10Router := V10Router{defaultRouter.Group("/api/v/1.0")}
+	v10Router.addV10Router()
 
-	for key, values := range r.Form {
-		log.Printf("Form field %q, Values %q\n", key, values)
- }
-
-	// create a new file in the local file system
-	f, err := os.OpenFile("./uploads/"+header.Filename, os.O_WRONLY|os.O_CREATE, 0666)
-	if err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-			return
-	}
-	defer f.Close()
-
-	// copy the uploaded file data to the new file
-	_, err = io.Copy(f, file)
-	if err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-			return
-	}
-
-	fStat, _ := f.Stat()
-
-	// return a success message to the client
-	fmt.Fprintf(w, "{'auth_token':'a48396e4f5bec65ddd415cb802cd37be7a5784cae', 'time':'%s', 'extra':'File uploaded successfully: file name is %v, file size is %v bytes'}", time.Now(), f.Name(), fStat.Size())
+  // Start the server
+	log.Println("Server start")
+  defaultRouter.Run(":5211")
 }
 
-type Person struct {
-	Name string
-	Age  int
-}
-
-func DisplayPersonHandler(w http.ResponseWriter, r *http.Request) {
-	var p Person
-
-	// 将请求体中的 JSON 数据解析到结构体中
-	// 发生错误，返回400 错误码
-	err := json.NewDecoder(r.Body).Decode(&p)
-	if err != nil {
-			http.Error(w, err.Error(), http.StatusBadRequest)
-			return
-	}
-
-	fmt.Fprintf(w, "Person: %+v", p)
-}
-
-func serveHome(w http.ResponseWriter, r *http.Request) {
-	log.Println(r.URL)
-	if r.URL.Path != "/" {
-		http.Error(w, "Not found", http.StatusNotFound)
-		return
-	}
-	http.ServeFile(w, r, "home.html")
-}
-
-func serveTest(w http.ResponseWriter, r *http.Request) {
-	log.Println(r.URL)
-	if r.URL.Path != "/test" {
-		http.Error(w, "Not found", http.StatusNotFound)
-		return
-	}
-
-	http.ServeFile(w, r, "test.html")
-}
-
-func DisplayFormDataHandler(w http.ResponseWriter, r *http.Request) {
-	r.ParseMultipartForm(1024*1024) 
-	if err := r.ParseForm(); err != nil {
-			panic(err)
-	}
-
-	for key, values := range r.Form {
-		log.Printf("Form field %q, Values %q\n", key, values)
- }
-
- fmt.Fprintf(w, "{'auth_token':'a48396e4f5bec65ddd415cb802cd37be7a5784cae', 'time':'%s'}", time.Now())
-}
-
-func appStartTime(w http.ResponseWriter, r *http.Request) {
-	r.ParseMultipartForm(1024*1024) 
-	if r.URL.Path != "/api/v/1.0/app/start/" {
-		http.Error(w, "Not found", http.StatusNotFound)
-		return
-	}
-
-	// for key,value:= range r.Header{
-	// 		log.Printf("%s=>%s\n",key,value)
-	// }
-
-	// for k, v := range r.URL.Query() {
-	// 	log.Printf("ParamName %q, Value %q\n", k, v)
-	// 	log.Printf("ParamName %q, Get Value %q\n", k, r.URL.Query().Get(k))
-	// }
-
-	for key, values := range r.Form {
-		log.Printf("Form field %q, Values %q\n", key, values)
- }
-
-	fmt.Fprintf(w, "{'auth_token':'a48396e4f5bec65ddd415cb802cd37be7a5784cae', 'time':'%s'}", time.Now())
-}
-
-func authLogin(w http.ResponseWriter, r *http.Request) {
-	r.ParseMultipartForm(1024*1024) 
-	if r.URL.Path != "/auth/token/login/" {
-		http.Error(w, "Not found", http.StatusNotFound)
-		return
-	}
-
-	for key, values := range r.Form {
-		log.Printf("Form field %q, Values %q\n", key, values)
- }
-
-	fmt.Fprintf(w, "{'auth_token':'a48396e4f5bec65ddd415cb802cd37be7a5784cae', 'time':'%s'}", time.Now())
+func addDefaultRouter(defaultRouter *gin.Engine, hub *Hub) {
+  // Serve static files from the Svelte build directory
+  defaultRouter.Use(static.Serve("/", static.LocalFile("./public", true)))
+	defaultRouter.GET("/ws", func(c *gin.Context) {
+		serveWs(hub, c.Writer, c.Request)
+	})
+	defaultRouter.GET("/hello.json", func(ctx *gin.Context) {
+		handleHello(ctx.Writer, ctx.Request)
+	})
+	defaultRouter.POST("/auth/token/login/", func(c *gin.Context) {
+		c.Request.ParseMultipartForm(1024*1024)
+		for key, values := range c.Request.PostForm {
+			log.Printf("Form field %q, Values %q\n", key, values)
+	  }
+	  c.JSON(http.StatusOK, getAnswer())
+	})
 }
 
 func handleHello(w http.ResponseWriter, r *http.Request) {
@@ -154,51 +63,7 @@ func handleHello(w http.ResponseWriter, r *http.Request) {
 	w.Write(res)
 }
 
-func handleSPA(w http.ResponseWriter, r *http.Request) {
-	http.ServeFile(w,r,"public/index.html")
-}
-
-func main() {
-	hub := newHub()
-	go hub.run()
-
-	// regist router
-	myrouter := MyRouter{mux.NewRouter()}
-
-	// Serve the static files for the Svelte app
-	myrouter.HandleFunc("/global.css", func(w http.ResponseWriter, r *http.Request) {
-		http.ServeFile(w,r,"public/global.css")
-	}, "GET")
-	myrouter.HandleFunc("/build/bundle.js", func(w http.ResponseWriter, r *http.Request) {
-		http.ServeFile(w,r,"public/build/bundle.js")
-	}, "GET")
-	myrouter.HandleFunc("/build/bundle.css", func(w http.ResponseWriter, r *http.Request) {
-		http.ServeFile(w,r,"public/build/bundle.css")
-	}, "GET")
-	myrouter.HandleFunc("/favicon.png", func(w http.ResponseWriter, r *http.Request) {
-		http.ServeFile(w,r,"public/favicon.png")
-	}, "GET")
-	
-	myrouter.HandleFunc("/", serveHome, "GET")
-	myrouter.HandleFunc("/test", serveTest, "GET")
-	myrouter.HandleFunc("/api/v/1.0/app/start/", appStartTime, "POST")
-	myrouter.HandleFunc("/display_form_data", DisplayFormDataHandler, "POST")
-	myrouter.HandleFunc("/parse_json_request", DisplayPersonHandler, "POST")
-	myrouter.HandleFunc("/upload_file", ReceiveFile, "POST")
-	myrouter.HandleFunc("/auth/token/login/", authLogin, "POST")
-	myrouter.HandleFunc("/api/v/1.0/run/start/", ReceiveFile, "POST")
-	myrouter.HandleFunc("/api/v/1.0/run/status/", DisplayFormDataHandler, "POST")
-	myrouter.HandleFunc("/api/v/1.0/run/finish/", ReceiveFile, "POST")
-	myrouter.HandleFunc("/ws", func(w http.ResponseWriter, r *http.Request) {
-			serveWs(hub, w, r)
-		}, "GET")
-
-	myrouter.HandleFunc("/hello.json", handleHello, "GET")
-	myrouter.HandleFunc("/svelte", handleSPA, "GET")
-
-	log.Println("Server start")
-	err := http.ListenAndServe(":5211", myrouter.router)
-	if err != nil {
-		log.Fatal("ListenAndServeTLS: ", err)
-	}
+func getAnswer() string {
+	ret := fmt.Sprintf("{'auth_token':'a48396e4f5bec65ddd415cb802cd37be7a5784cae', 'time':'%s'}", time.Now())
+	return ret
 }
